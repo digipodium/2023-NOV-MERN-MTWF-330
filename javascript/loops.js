ba7a074861df8fd0a53fd071ef3a519cb9940b58
m-triple-m <triplem656@gmail.com>
2023-11-28 17:00:30 +0530

    ..

---
+++ javascript/loops.js
@@ -0,0 +1,11 @@
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+
+for(let i=1; i<=100; i++){
+    if(i%5 === 0 )
+    console.log(i);
+}
+
+// WAP to print all the numbers which are divisible by 3 but not by 6
+// in range of 50 - 200
