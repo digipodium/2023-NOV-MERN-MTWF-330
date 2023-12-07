d80e26eddc1e68c4dfd789c56169f1ea3d49fa40
m-triple-m <triplem656@gmail.com>
2023-12-07 18:19:39 +0530

    components

---
+++ Section 2/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+const App = () => {
+  
+  return (
+    <div>
+      <Home />
+      <Login />
+      <Signup />
+    </div>
+  )
+}
+
+export default App;
---
+++ Section 2/frontend/src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h2 style={{ backgroundColor : 'yellow', fontSize: 50 }}>Login Form</h2>
+        <button className='btn btn-primary'>NIce</button>
+    </div>
+  )
+}
+
+export default Login
