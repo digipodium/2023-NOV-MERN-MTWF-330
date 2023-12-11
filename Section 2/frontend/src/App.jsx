486edd0abf4fec17b15ba0ea3da135f128b6a479
m-triple-m <triplem656@gmail.com>
2023-12-11 17:01:21 +0530

    routing and navigation

--- Section 2/frontend/src/App.jsx
+++ Section 2/frontend/src/App.jsx
@@ -3,14 +3,29 @@ import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
 
 const App = () => {
   
   return (
     <div>
-      <Home />
-      <Login />
-      <Signup />
+      <BrowserRouter>
+
+        {/* <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link> */}
+
+        <Navbar />
+
+        <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='login' element={ <Login /> } />
+            <Route path='signup' element={ <Signup /> } />
+            <Route path='contact' element={ <Contact /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
