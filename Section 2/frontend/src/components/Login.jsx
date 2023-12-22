0e29876b5bbaddfed3961bbe9f753136b56f3499
m-triple-m <triplem656@gmail.com>
2023-12-22 17:00:33 +0530

    formik

--- Section 2/frontend/src/components/Login.jsx
+++ Section 2/frontend/src/components/Login.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+
 
 const Login = () => {
   return (
-    <div>
-        <h2 style={{ backgroundColor : 'yellow', fontSize: 50 }}>Login Form</h2>
-        <button className='btn btn-primary'>NIce</button>
+    <div className='vh-100'>
+        <div className='container py-5 h-100'>
+          <div className="col-md-3 mx-auto">
+              <div className='card rounded-3'>
+                <div className="card-body">
+                  <h3 className='text-center my-3'>Login Form</h3>
+                  <form>
+                    <label htmlFor="">Email Address</label>
+                    <input type="text" className='form-control mb-4' />
+                    <label htmlFor="">Password</label>
+                    <input type="password" className='form-control mb-4' />
+
+                    <button className='btn btn-dark mt-4'>Submit</button>
+                  </form>
+                </div>
+              </div>
+          </div>
+          <div className="col-md-9"></div>
+        </div>
     </div>
   )
 }
--- Section 2/frontend/src/components/Signup.jsx
+++ Section 2/frontend/src/components/Signup.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import { useFormik } from 'formik';
+import React from 'react';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  name: Yup.string().required('Name is Required').min(4, 'Name is too short'),
+  email: Yup.string().email('Invalid email').required('Required'),
+  password : Yup.string().required('Password is Required').min(8, 'Password is too short')
+});
 
 const Signup = () => {
+
+  const signupForm = useFormik({
+    initialValues: {
+      name: '',
+      email : '',
+      password : ''
+    },
+    onSubmit: ( values ) => { console.log(values); },
+    validationSchema: SignupSchema
+  });
+
   return (
     <div className='vh-100 bg-body-secondary'>
       <div className='col-md-4 mx-auto py-5'>
         <div className="card">
           <div className="card-body p-4">
-            <form>
+            <form onSubmit={ signupForm.handleSubmit }>
               <h2 className='text-center'>Register Form</h2>
               <hr />
 
               <label htmlFor="">Name</label>
-              <input type="text" className='form-control mb-4' />
+              <span className='ms-4 fs-6 text-danger'>{signupForm.errors.name}</span>
+              <input type="text" className='form-control mb-4' id="name" onChange={ signupForm.handleChange } value={signupForm.values.name} />
               <label htmlFor="">Email Address</label>
-              <input type="text" className='form-control mb-4' />
+              <input type="text" className='form-control mb-4' id="email" onChange={ signupForm.handleChange } value={signupForm.values.email}  />
               <label htmlFor="">Password</label>
-              <input type="password" className='form-control mb-4' />
+              <input type="password" className='form-control mb-4' id='password' onChange={signupForm.handleChange} value={signupForm.values.password} />
 
-              <button className='btn btn-dark w-100 mt-5'>Submit</button>
+              <button type='submit' className='btn btn-dark w-100 mt-5'>Submit</button>
             </form>
           </div>
         </div>
